06f42cde80d09a9dc56e2af5f7d4cf9278250983
MikeKomari <michaelk.projects@gmail.com>
2024-06-02 09:05:36 +0700

    temp

--- src/js/Accounts.js
+++ src/js/Accounts.js
@@ -1,5 +1,5 @@
 //Not login === 0, login === 1
-let accountState = 0;
+let accountState = 1;
 
 export function accountStateLogin() {
   accountState = 1;
@@ -17,20 +17,42 @@ console.log(accountState);
 //navbar change
 const navAccount = document.querySelector(".navbar__account");
 const loginRegisterButton = document.querySelectorAll(".buttonOne");
-const accountIcon = document.querySelector(".userPNG");
+const accountIcon = document.querySelectorAll(".userPNG");
+const accountDropDown = document.querySelectorAll(".accountFeatureContainer");
+const logOutButton = document.querySelectorAll(".logOutButton");
 function navChange() {
   if (accountState === 1) {
     loginRegisterButton.forEach((button) => {
       button.classList.add("hidden");
     });
-    accountIcon.classList.remove("hidden");
+    accountIcon.forEach((icon) => {
+      icon.classList.remove("hidden");
+    });
   } else {
-    accountIcon.classList.add("hidden");
+    // accountIcon.classList.add("hidden");
     loginRegisterButton.forEach((button) => {
       button.classList.remove("hidden");
     });
+    accountIcon.forEach((icon) => {
+      icon.classList.add("hidden");
+    });
   }
 }
+
+accountIcon.forEach((click) => {
+  click.addEventListener("click", () => {
+    accountDropDown.forEach((drop) => {
+      drop.classList.toggle("hidden");
+    });
+  });
+});
+
+logOutButton.forEach((click) => {
+  click.addEventListener("click", () => {
+    accountStateLogout();
+    navChange();
+  });
+});
 
 navChange();
 
